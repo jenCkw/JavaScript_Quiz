e300f20dadacfa52db59b6da8edd6d907a547a9b
jenCkw <cikwaninejenny@gmail.com>
2022-07-20 15:02:41 +0100

    refactor: code review updated

--- script.js
+++ script.js
@@ -103,11 +103,7 @@ class QUIZ {
    * @returns {Boolean}
    */
   isEnd() {
-    if (this.questionIndex == this.questions.length) {
-      return true;
-    } else {
-      return false;
-    }
+    return this.questionIndex == this.questions.length;
   }
   /**
    * Affiche une question
@@ -249,114 +245,121 @@ class Question {
 }
 
 let questions = [
-  // new Question(
-  //   "Javascript is an ______ language ?",
-  //   ["Object-Oriented", "Object-Based", "Procedural", "None of the above"],
-  //   "Object-Oriented"
-  // ),
-  new Question(
-    "Which of the following keywords is used to define a variable in Javscript ?",
-    ["var", "let", "Both A and B", "None of the above"],
-    "Both A and B"
-  ),
-  new Question(
-    "How do we write a comment in javscript?",
-    ["/**/", "//", "#", "$$"],
-    "//"
-  ),
-  new Question(
-    "What will be the output of the following code snippet: print(typeof(NaN); ?",
-    ["object", "Number", "String", "None of the above"],
-    "Number"
-  ),
-  new Question(
-    "JSON stands for what?",
-    [
+  {
+    question:
+      "Which of the following keywords is used to define a variable in Javscript ?",
+    choix: ["var", "let", "Both A and B", "None of the above"],
+    reponse: "Both A and B",
+  },
+
+  {
+    question: "How do we write a comment in javscript?",
+    choix: ["/**/", "//", "#", "$$"],
+    reponse: "//",
+  },
+
+  {
+    question:
+      "What will be the output of the following code snippet: print(typeof(NaN); ?",
+    choix: ["object", "Number", "String", "None of the above"],
+    reponse: "Number",
+  },
+
+  {
+    question: "JSON stands for what?",
+    choix: [
       "Javascript Object Notation",
       "Java Script Number",
       "JavaScript Script Notice",
       "JavaScript Script Note",
     ],
-    "Javascript Object Notation"
-  ),
-  new Question(
-    "How to declare a variable in js ?",
-    ["let", "echo", "print", "console.log()"],
-    "let"
-  ),
-  new Question(
-    "which of the following methods is used to access HTML elements using JavaScript ?",
-    [
+    reponse: "Javascript Object Notation",
+  },
+
+  {
+    question: "How to declare a variable in js ?",
+    choix: ["let", "echo", "print", "console.log()"],
+    reponse: "let",
+  },
+
+  {
+    question:
+      "which of the following methods is used to access HTML elements using JavaScript ?",
+    choix: [
       "getElementbyId()",
       "getElementbyClassName()",
       "Both A and B",
       "None of the above",
     ],
-    "Both A and B"
-  ),
-  new Question(
-    "What keywords is used to chek whether a given property is valid or not ?",
-    ["in", "is in", "xists", "lies"],
-    "in"
-  ),
-  new Question(
-    "How can a datatype be declared to be a constant type ?",
-    ["const", "var", "let", "constant"],
-    "const"
-  ),
-  new Question(
-    "Which of the following methods can be used to display data in some form using Javascript ?",
-    [
+    reponse: "Both A and B",
+  },
+  {
+    question:
+      "What keywords is used to chek whether a given property is valid or not ?",
+    choix: ["in", "is in", "xists", "lies"],
+    reponse: "in",
+  },
+  {
+    question: "How can a datatype be declared to be a constant type ?",
+    choix: ["const", "var", "let", "constant"],
+    reponse: "const",
+  },
+  {
+    question:
+      "Which of the following methods can be used to display data in some form using Javascript ?",
+    choix: [
       "document.writer()",
       "console.log()",
       "window.alert()",
       "all of the above",
     ],
-    "all of the above"
-  ),
-  new Question(
-    "What does the 'toLocateString() method do in JS ?",
-    [
+    reponse: "all of the above",
+  },
+  {
+    question: "What does the 'toLocateString() method do in JS ?",
+    choix: [
       "Returns a localised object representation",
       "Returns a parsed string",
       "Returns a localised string representation of an object",
       "Non of the above",
     ],
-    "Returns a localised string representation of an object"
-  ),
-  new Question(
-    "What does the Javascript 'debugger' statement do ?",
-    [
+    reponse: "Returns a localised string representation of an object",
+  },
+  {
+    question: "What does the Javascript 'debugger' statement do ?",
+    choix: [
       "it will debug all the errors in the programm at runtime",
       "it acts as a beakpoint in a program",
       "it will debug error in the curennt statement if any",
       "all of tquestion_templatehe above",
     ],
-    "it acts as a beakpoint in a program"
-  ),
-  new Question(
-    "Upon encountering empty statements, what does the Javascript interpreter do?",
-    [
+    reponse: "it acts as a beakpoint in a program",
+  },
+  {
+    question:
+      "Upon encountering empty statements, what does the Javascript interpreter do?",
+    choix: [
       "Throws an error",
       "Ignores the statements",
       "Gives a warning",
       "None of the above",
     ],
-    "Ignores the statements"
-  ),
-  new Question(
-    "What is the use of the noscript tag in Javscrpt ?",
-    [
+    reponse: "Ignores the statements",
+  },
+  {
+    question: "What is the use of the noscript tag in Javscrpt ?",
+    choix: [
       "The contents are displayed by non-JS-based browsers",
       "Clears all the cookies and cache",
       "Both A and B",
       "None of the above",
     ],
-    "The contents are displayed by non-JS-based browsers"
-  ),
-  new Question(
-    "Hows to stop an interval timer in javascript ?",
-    ["clearInterval", "clearTimer", "intervalOver", "None of the above"],
-    "clearInterval"
-  ),
+    reponse: "The contents are displayed by non-JS-based browsers",
+  },
+
+  {
+    question: "Hows to stop an interval timer in javascript ?",
+    choix: ["clearInterval", "clearTimer", "intervalOver", "None of the above"],
+    reponse: "clearInterval",
+  },
 ];
